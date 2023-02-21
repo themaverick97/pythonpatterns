15270f05ae665f5831564e015db22aeb4b6db4c4
muralidhar <muralid663@gmail.com>
2023-02-21 12:27:43 +0530

    final

---
+++ pattern2.py
@@ -0,0 +1,13 @@
+
+i = 1
+while i <= 5:
+    j = i
+    while j < 5:
+        print(' ', end=' ')
+        j += 1
+    k = 1
+    while k <= i:
+        print('*', end=' ')
+        k += 1
+    print()
+    i += 1
